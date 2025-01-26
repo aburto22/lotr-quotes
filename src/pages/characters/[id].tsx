459fe49a51e1e9e7ffbfbad53cd25013ba85681d
Alejandro Aburto Salazar <aburto22@gmail.com>
2025-01-26 19:59:40 +0100

    Replace image with legacy image

--- src/pages/characters/[id].tsx
+++ src/pages/characters/[id].tsx
@@ -6,7 +6,7 @@ import type {
 import type { ParsedUrlQuery } from "querystring";
 import { colors } from "@styles/cssVariables";
 import Layout from "@components/Layout";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 import { getCharacterById, getCharactersId, getWikiImg } from "@lib/ssg";
 import type { Character } from "@lib/ssg";
